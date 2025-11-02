4d4a146328d63a0353284a0b16f6a8f59629295d
Achyuth Parisha <achuthparisha005@gmail.com>
2025-11-03 05:08:49 +0530

    fix: Render frontend env and axios baseURL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://mern-task-manager-backend.onrender.com/api" // 🔥 your deployed backend URL
-      : "http://localhost:6969/api", // for local dev
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
